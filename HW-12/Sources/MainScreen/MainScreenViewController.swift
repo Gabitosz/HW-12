9c1fef24951c6da42c8036eca482c596162faa6d
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-09-27 21:09:57 +0300

    Add timer label

--- HW-12/Sources/MainScreen/MainScreenViewController.swift
+++ HW-12/Sources/MainScreen/MainScreenViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class MainScreenViewController: UIViewController {
 
+    // MARK: Outlets
+    
+    private lazy var timerLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Time"
+        label.textColor = .black
+        label.font = .boldSystemFont(ofSize: 60)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupHierarchy()
+        setupLayout()
     }
 
+    // MARK: Setup
+    
+    private func setupHierarchy() {
+        view.addSubview(timerLabel)
+    }
 
+    private func setupLayout() {
+        timerLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            timerLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        
+        
+        ])
+        
+    }
 }
 
