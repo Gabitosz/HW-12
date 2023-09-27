84b2f6138e639c33956d967d7dcdc6566a5dd6f6
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-09-27 23:24:13 +0300

    Add pause button

--- HW-12/Sources/MainScreen/MainScreenViewController.swift
+++ HW-12/Sources/MainScreen/MainScreenViewController.swift
@@ -16,12 +16,13 @@ class MainScreenViewController: UIViewController {
     
     private lazy var timerLabel: UILabel = {
        let label = UILabel()
-        label.text = "25"
+        label.text = "25 sec."
         label.textColor = .black
         label.font = .boldSystemFont(ofSize: 60)
         label.textAlignment = .center
         return label
     }()
+    
     
     private lazy var timerStartButton: UIButton = {
         let button = UIButton()
@@ -30,7 +31,20 @@ class MainScreenViewController: UIViewController {
             let changedColorImage = image.withTintColor(.green, renderingMode: .alwaysOriginal)
             button.setImage(changedColorImage, for: .normal)
         }
-        button.addTarget(self, action: #selector(timerButtonPressed), for: .touchUpInside)
+        button.addTarget(self, action: #selector(timerStartButtonPressed), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var timerPauseButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "pause"), for: .normal)
+        if let image = button.currentImage {
+            let changedColorImage = image.withTintColor(.red, renderingMode: .alwaysOriginal)
+            button.setImage(changedColorImage, for: .normal)
+        }
+        button.addTarget(self, action: #selector(timerPauseButtonPressed), for: .touchUpInside)
+        button.isHidden = true
         return button
     }()
     
@@ -48,37 +62,57 @@ class MainScreenViewController: UIViewController {
     private func setupHierarchy() {
         view.addSubview(timerLabel)
         view.addSubview(timerStartButton)
+        view.addSubview(timerPauseButton)
     }
 
     private func setupLayout() {
         timerLabel.translatesAutoresizingMaskIntoConstraints = false
         timerStartButton.translatesAutoresizingMaskIntoConstraints = false
+        timerPauseButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             timerLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             
             timerStartButton.topAnchor.constraint(equalTo: timerLabel.bottomAnchor, constant: 30),
-            timerStartButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -165),
+            timerStartButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -175),
             timerStartButton.widthAnchor.constraint(equalToConstant: 50),
-            timerStartButton.heightAnchor.constraint(equalToConstant: 50)
+            timerStartButton.heightAnchor.constraint(equalToConstant: 50),
             
+            timerPauseButton.topAnchor.constraint(equalTo: timerLabel.bottomAnchor, constant: 20),
+            timerPauseButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -165),
+            timerPauseButton.widthAnchor.constraint(equalToConstant: 70),
+            timerPauseButton.heightAnchor.constraint(equalToConstant: 70)
         
         ])
     }
     
     // MARK: - Actions
     
-    @objc private func timerButtonPressed() {
+    @objc private func timerStartButtonPressed() {
             countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timerStartButton.isEnabled = false
+        timerStartButton.isHidden = true
+        timerPauseButton.isEnabled = true
+        timerPauseButton.isHidden = false
     }
+    
+    @objc private func timerPauseButtonPressed() {
+           countdownTimer?.invalidate()
+        timerStartButton.isEnabled = true
+        timerStartButton.isHidden = false
+        timerPauseButton.isEnabled = false
+        timerPauseButton.isHidden = true
+       }
     
     @objc private func updateTimer() {
         if secondsRemaining > 0 {
             secondsRemaining -= 1
-            timerLabel.text = "\(secondsRemaining)"
+            let minutes = secondsRemaining / 60
+            let seconds = secondsRemaining % 60
+            timerLabel.text = String(format: "%02d:%02d", minutes, seconds)
         } else {
             // Таймер завершен
-            timerLabel.text = "0"
+            timerLabel.text = "0 sec."
             countdownTimer?.invalidate()
         }
     }
